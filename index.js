65e4e8cd6a86401f575f0fe524718164d222f9b2
Andrew Friend <andrewfriend53@gmail.com>
2021-05-01 22:08:31 +0100

    initial commit, undoccumented function

---
+++ index.js
@@ -0,0 +1,26 @@
+function connectiveList(items,connective){
+    if(!items.length){
+        throw "items must be an array"
+    }
+    //allow non english users to set own connective otherwise default to and
+    if(!connective){
+        connective = "and"
+    }
+
+    if(items.length==1){
+        return items[0]
+    }
+    else{
+        var string = "";
+        //append all the items bar the final two
+        for(var i = 0; i < items.length-2;i++)
+        {
+            string += items[i]+", ";
+        }
+        //now append the final two
+        string += items[items.length-2] + " and " + items[items.length-1];
+        return string
+    }
+}
+
+module.exports = connectiveList;
